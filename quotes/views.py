98694b0a355fc454d9ae3d9d65c2269e41f402cf
ladygitcomits <faithwanjiiri@gmail.com>
2026-03-26 10:49:25 +0300

    Add comments to explain code

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import random
-
+# This function handles the homepage
 def home(request):
+    # A list of motivational quotes to display randomly
     quotes = [
         {"text": "Stay consistent.", "author": "You"},
         {"text": "Keep going.", "author": "Unknown"},
@@ -9,4 +10,5 @@ def home(request):
     ]
 
     quote = random.choice(quotes)
+    # Render the home.html template and pass the chosen quote
     return render(request, 'home.html', {'quote': quote})
